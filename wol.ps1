6dc3afc40adbf29079564885271ed072228d81f7
test <test>
2025-08-22 16:07:51 +0200

    first commit

---
+++ wol.ps1
@@ -0,0 +1,41 @@
+
+function Send-WakeOnLan {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$MacAddress,
+
+        [string]$Broadcast = "192.168.0.255",
+        [int]$Port = 9
+    )
+
+    # Clean up MAC (remove separators)
+    $mac = $MacAddress -replace "[:\-\.]", ""
+    if ($mac.Length -ne 12) {
+        throw "Invalid MAC address format"
+    }
+
+    # Convert MAC string to byte array
+    $macBytes = for ($i=0; $i -lt 12; $i+=2) {
+        [byte]::Parse($mac.Substring($i,2), "HexNumber")
+    }
+
+    # Build magic packet
+    $packet = (,[byte]0xFF * 6) + ($macBytes * 16)
+
+    # Create UDP client and send
+    $udp = New-Object System.Net.Sockets.UdpClient
+    $udp.EnableBroadcast = $true
+    $udp.Connect($Broadcast, $Port)
+    $udp.Send($packet, $packet.Length) | Out-Null
+    $udp.Close()
+    Write-Host $Broadcast
+}
+
+$MacAddresses = @(
+    'Zero'
+)
+#Send-WakeOnLan -MacAddress "30-5A-3A-55-FA-E6"
+
+$MacAddresses | ForEach-Object {
+    Write-Host $_
+}
